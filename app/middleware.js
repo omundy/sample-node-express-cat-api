47836616b851719c4833399e3f124aadc8149b96
Owen Mundy <omundy@gmail.com>
2023-01-31 08:04:07 -0500

    Adds rename files middleware for demo

--- app/middleware.js
+++ app/middleware.js
@@ -2,22 +2,83 @@
  *	Middleware module - helper functions for your application
  */
 
+var exports = (module.exports = {});
 
-var exports = module.exports = {};
-
-
-
-// function to show all requests
+/**
+ * 	Middleware - show all requests
+ */
 exports.showRequests = (req, res, next) => {
 	// ignore static files
-	if (!req.originalUrl.includes("/assets/") && !req.originalUrl.includes("/favicon")) {
+	if (
+		!req.originalUrl.includes("/assets/") &&
+		!req.originalUrl.includes("/favicon")
+	) {
 		// log the path
-		console.log(`\nRequest ${req.method} ${req.originalUrl} [STARTED] from ${req.ip}`, new Date().toLocaleString());
+		console.log(
+			`\nRequest ${req.method} ${req.originalUrl} [STARTED] from ${req.ip}`,
+			new Date().toLocaleString()
+		);
 
-		res.on('close', () => {
-			console.log(`Request ${req.method} ${req.originalUrl} [CLOSED] from ${req.ip}`, new Date().toLocaleString());
+		res.on("close", () => {
+			console.log(
+				`Request ${req.method} ${req.originalUrl} [CLOSED] from ${req.ip}`,
+				new Date().toLocaleString()
+			);
 		});
 	}
 	// invoke next middleware function
 	next();
 };
+
+/**
+ * 	Middleware - rename all files
+ */
+exports.renameFiles = async (req, res, next) => {
+	let count = await renameAllFiles();
+	console.log(count);
+	req.count = count;
+	// invoke next middleware function
+	next();
+};
+
+
+
+
+const fs = require("fs").promises;
+
+/**
+ * 	Rename all files in a dir
+ */
+async function renameAllFiles() {
+	let path = "public/assets/img/" || __dirname;
+	let count = 0;
+	let files = await readdir(path);
+
+	files.forEach(async (file, index) => {
+		// console.log(file);
+		let r = Math.random(); // temp name
+		// rename file
+		fs.rename(
+			`${path}${file}`,
+			`${path}temp-name-${r}-${String(index).padStart(2, "0")}.jpg`
+		);
+		count++;
+		// console.log(`${count} ${file} renamed`);		
+	});
+	if (count == files.length) {
+		console.log(`${count} files renamed`);
+		return count;
+	}
+}
+
+/**
+ * 	Return all filenames in a dir
+ */
+const readdir = (path) => {
+	return fs.readdir(path).then(
+		(
+			list // remove hidden files from list
+		) => list.filter((item) => !/(^|\/)\.[^/.]/g.test(item))
+	);
+};
+
--- app/routes.js
+++ app/routes.js
@@ -2,33 +2,43 @@
  *	Routes module - all the website and api endpoints
  */
 
+// load middleware
+const Middleware = require("./middleware.js");
 
-module.exports = function(app) {
 
-
+module.exports = function (app) {
 	/////////// WEBSITE ROUTES ///////////
 
 	// website home page
-	app.get('/', (req, res) => {
+	app.get("/", (req, res) => {
 		// random # between 1-5
 		let r = Math.floor(Math.random() * 5);
 		// render page
-		res.render('home', {
-			title: 'home page',
-			img: 'cat' + r + '.jpg'
+		res.render("home", {
+			title: "home page",
+			img: "cat" + r + ".jpg",
 		});
 	});
-
 
 	/////////// API ROUTES ///////////
 
 	// api endpoint
-	app.get('/api', (req, res) => {
+	app.get("/api", (req, res) => {
 		res.status(200).json({
-			message: "hello"
+			message: "hello",
 		});
 	});
 
+	/////////// UTILITY ROUTES ///////////
 
+	// // change the names of all the cat files to make them easy to reference
+	// app.get("/rename",  Middleware.renameFiles, (req, res) => {
+	// 	try {
+	// 		res.send(req.count + " files were renamed");
+	// 	} catch (e) {
+	// 		console.error(e);
+	// 	}
+	// });
 
+	
 };
